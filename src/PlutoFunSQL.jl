ebf928db42a669654e7b33c4c912ef714a1efc95
Kyrylo Simonov <xi@resolvent.net>
2025-06-15 13:18:38 -0500

    DBInterface.Connection middleware that returns DataFrame as a cursor

--- src/PlutoFunSQL.jl
+++ src/PlutoFunSQL.jl
@@ -9,12 +9,13 @@ using UUIDs
 using AbstractPlutoDingetjes
 using HTTP
 
+include("connection.jl")
+include("format.jl")
 include("resolve.jl")
 include("summary.jl")
 include("validate.jl")
 include("drawio.jl")
 include("layout.jl")
-include("format.jl")
 
 # keep last; export all funsql_
 include("duckdb.jl")
